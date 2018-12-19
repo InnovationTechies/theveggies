d958e544cb27ee3f8688bb2fa7c388809a77d739
cweng <cweng@ZOZO>
2018-12-19 14:22:40 +0200

    Add project files.

---
+++ veggies/Models/ProductCatagory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace veggies.Models
+{
+    public class ProductCatagory
+    {
+        [Key]
+        public int catagoryID { get; set; }
+        [Display(Name = "Name")]
+        public string CatagoryName { get; set; }
+        [Display(Name = "Description")]
+        public string CatagoryDesc { get; set; }
+
+        //public ICollection<PnpProducts> Products { get; set; }
+    }
+}
---
+++ veggies/Models/Products.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace veggies.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Display(Name = "Name")]
+        public string ProductName { get; set; }
+        [Display(Name = "Price")]
+        public float ProductPrice { get; set; }
+        [Display(Name = "Image")]
+        public byte[] ProductImage { get; set; }
+
+        //[Display(Name = "Percent")]
+        //public double ProductDropPercent { get; set; }
+        //[Display(Name = "Description")]
+        //public string ProductDesc { get; set; }
+        //[Display(Name = "SpecialEndDate")]
+        //public DateTime ProductDateEndPromo { get; set; }
+        //[Display(Name = "Catagory")]
+        ////public IEnumerable<ProductCatagory> productCatagories { get; set; }
+
+        //public ProductCatagory Catagory { get; set; }
+
+        //public ICollection<PnpSales> Sales { get; set; }
+
+        //public List<ProductCatagory> productCatagoy { get; set; }
+
+
+
+
+    }
+}
